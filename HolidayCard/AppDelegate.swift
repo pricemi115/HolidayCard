38e1e73985c4504fd2db07255158bd2be10189f8
Mike Price <pricemi115@users.noreply.github.com>
2017-12-22 20:30:01 -0500

    Initial Commit

---
+++ HolidayCard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HolidayCard
+//
+//  Created by Michael Price on 12/22/17.
+//  Copyright © 2017 GrumpTech. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HolidayCard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HolidayCard
+//
+//  Created by Michael Price on 12/22/17.
+//  Copyright © 2017 GrumpTech. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
